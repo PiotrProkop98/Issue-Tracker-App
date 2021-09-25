196dd13f593a463ea2240bc1881287d1fd8eef6f
unknown <piotrprokop98@gmail.com>
2021-09-25 20:44:14 +0200

    Working on models and seeders.

---
+++ database/migrations/2021_09_25_165939_create_user_project_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserProjectTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_project', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->unsigned();
+            $table->integer('project_id')->unsigned();
+            $table->string('role', 255)->default('developer');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_project');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,29 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProjectController;
 
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+*/
+
+Route::get('/users', function (Request $request) {
+    $users = User::all();
+    return response()->json($users, 200);
+});
+
+Route::get('/users-projects', function () {
+    $users_projects = DB::table('user_project')->get();
+    return response()->json($users_projects, 200);
 });
 
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
 Route::post('/register', [UserController::class, 'register']);
+
+Route::get('/projects', [ProjectController::class, 'index']);
