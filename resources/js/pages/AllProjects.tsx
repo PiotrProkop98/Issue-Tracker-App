c18bfce144340ab02f0eb2c15964ba3290e1cac9
unknown <piotrprokop98@gmail.com>
2021-10-05 13:04:33 +0200

    Initial work on AllProjects page.

--- resources/js/pages/AllProjects.tsx
+++ resources/js/pages/AllProjects.tsx
@@ -1,10 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import { CircularProgress, Container, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+
+interface ProjectsState {
+    data: [
+        {
+            created_at: string,
+            updated_at: string,
+            id: number,
+            is_private: number,
+            name: string
+        }
+    ]
+};
 
 const AllProjects = () => {
+    const [projects, setProjects] = useState<ProjectsState>();
+    const [isLoading, setIsloading] = useState<boolean>(true);
+
+    useEffect(() => {
+        setIsloading(true);
+
+        axios.get('http://localhost:8100/api/projects')
+            .then(response => {
+                console.log(response.data);
+                setProjects(response.data);
+                setIsloading(false);
+            })
+            .catch(err => console.error(err));
+    }, []);
+
+    let projectsJsx;
+
+    if (isLoading) {
+        projectsJsx = <CircularProgress />;
+    } else {
+        projectsJsx = (
+            <TableContainer sx={{ minWidth: '350px' }} component={Paper}>
+                <Table size="small" aria-label="public projects">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Project name</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        { projects!.data.map((project, i) => (
+                            <TableRow key={i} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                                <TableCell component="th" scope="row">
+                                    { project.name }
+                                </TableCell>
+                            </TableRow>
+                        )) }
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        );
+    }
+
     return (
-        <div>
-            All Projects here...
-        </div>
+        <Container maxWidth="xs">
+            { projectsJsx }
+        </Container>
     );
 };
 
