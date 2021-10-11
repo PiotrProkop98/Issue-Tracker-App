e174ae474a69f278e7e170c2c3ff1109c6bfd174
unknown <piotrprokop98@gmail.com>
2021-10-11 16:41:01 +0200

    Working on AllProjects button group pagination.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,7 +28,7 @@ class DatabaseSeeder extends Seeder
         ]);
 
         Project::factory()
-            ->count(30)
+            ->count(60)
             ->has(User::factory()->count(3))
             ->create();
         
--- resources/js/pages/AllProjects.tsx
+++ resources/js/pages/AllProjects.tsx
@@ -8,7 +8,7 @@ import {
     ButtonGroup, 
     CircularProgress, 
     Container, 
-    Grid, 
+    Grid,
     Paper, 
     Table, 
     TableBody, 
@@ -22,25 +22,35 @@ import {
 import { fetchProjects } from '../store/projects';
 import { RootState } from '../store';
 
+const modalStyle = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
 const AllProjects = () => {
     const dispatch = useAppDispatch();
     const { projects, last_page, loaded } = useSelector((state: RootState) => state.projectSlice);
 
     const [isLoading, setIsloading] = useState<boolean>(true);
     const [currentPage, setCurrentPage] = useState<string>('1');
-    const [lastPage, setLastPage] = useState<number>(1);
 
     const [projectsJsx, setProjectsJsx] = useState<any>();
     const [prevButtonJsx, setPrevButtonJsx] = useState<any>();
     const [nextButtonJsx, setNextButtonJsx] = useState<any>();
+    const [pageButtonsJsx, setPageButtonsJsx] = useState<any>();
 
     useEffect(() => {
         setCurrentPage('1');
     }, []);
 
     useEffect(() => {
-        setLastPage(Number(last_page));
-
         setIsloading(true);
 
         dispatch(fetchProjects(Number(currentPage)))
@@ -57,7 +67,7 @@ const AllProjects = () => {
     };
 
     const handleNext = () => {
-        if (currentPage === String(lastPage)) return;
+        if (currentPage === last_page) return;
         setCurrentPage(String(Number(currentPage) + 1));
     };
 
@@ -93,11 +103,38 @@ const AllProjects = () => {
             setPrevButtonJsx(<Button onClick={handlePrev}>Prev</Button>);
         }
 
-        if (currentPage === String(lastPage)) {
+        if (currentPage === String(last_page)) {
             setNextButtonJsx(<Button disabled>Next</Button>);
         } else {
             setNextButtonJsx(<Button onClick={handleNext}>Next</Button>);
         }
+
+        let buttonsJsx: any[];
+
+        if (last_page === '1') {
+            buttonsJsx = [];
+        } else if (last_page === '2') {
+            buttonsJsx = [
+                <Button key={1} onClick={() => setCurrentPage('1')}>1</Button>,
+                <Button key={2} onClick={() => setCurrentPage('2')}>2</Button>
+            ];
+        } else if (last_page === '3') {
+            buttonsJsx = [
+                <Button key={1} onClick={() => setCurrentPage('1')}>1</Button>,
+                <Button key={2} onClick={() => setCurrentPage('2')}>2</Button>,
+                <Button key={3} onClick={() => setCurrentPage('3')}>3</Button>
+            ]
+        } else {
+            buttonsJsx = [
+                <Button key={1} onClick={() => setCurrentPage('1')}>1</Button>,
+                <Button key={2} onClick={() => setCurrentPage('2')}>2</Button>,
+                <Button key={3} onClick={() => setCurrentPage('3')}>3</Button>,
+                <Button key={4}>...</Button>,
+                <Button key={5} onClick={() => setCurrentPage(last_page)}>{ last_page }</Button>
+            ]
+        }
+
+        setPageButtonsJsx(buttonsJsx);
     }, [isLoading]);
 
     return (
@@ -107,6 +144,7 @@ const AllProjects = () => {
                 <Grid container direction="column" justifyContent="center" alignItems="center">
                     <ButtonGroup variant="contained" aria-label="navigation buttons">
                         { prevButtonJsx }
+                        { pageButtonsJsx.map((btn: any) => btn) }
                         { nextButtonJsx }
                     </ButtonGroup>
                     <Typography mt={1} variant="h6" component="div">
